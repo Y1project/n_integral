6ebe5042c8c7384f1d3dbacff4368ac556477ef6
eb-lad <66913898+eb-lad@users.noreply.github.com>
2020-06-14 16:33:01 +0100

    Add files via upload

---
+++ code to resolve expected bubble intigral.py
@@ -0,0 +1,44 @@
+import scipy as sp
+from scipy import special
+
+import scipy.integrate as integrate
+
+import matplotlib.pyplot as plt
+import math
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+#define constants
+a_inf =  1.091*10**(-29)
+a_o = 1
+H_o = 1.5*10**(-42)
+m = 1.8*10**13
+bar = 3.46e38
+
+H = 1.1937e8 
+
+eta_o_eta_inf = 3.21/(a_o * H_o)
+
+def eta_inf_eta_n(n):
+    coeff = 1/(a_inf* m)*(3 * sp.pi/2*sp.e)**0.5
+    erfi_1 = (n + 1/2 )**0.5
+    erfi_2 = sp.sqrt(0.5)
+    return coeff*(special.erfi(erfi_1) - special.erfi(erfi_2))
+
+def eta_o_eta_N(n):
+    return eta_o_eta_inf + eta_inf_eta_n(n)
+
+#this is a fucntion descibing the inside of the intigral
+#the constants outside the initgral are not concidered
+
+def integral(N):
+    return (1+2*N)**(3/2) * math.exp(-3*N) * eta_o_eta_N(N)
+
+
+
+#%%
+#Resolve the intigral - as in terms of e-foldings (N) we only concider from 0 to 60
+
+result_0_60 = sp.integrate.quad(integral, 0, 60)
+
+print(result_0_60[0])
